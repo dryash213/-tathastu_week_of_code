98b122a1a1d30e9fff619bf92f1cca60e192c930
Yash Kushwaha <51244820+dryash213@users.noreply.github.com>
2020-05-31 23:31:04 +0530

    Delete program1.py

--- program1.py
+++
@@ -1,7 +0,0 @@
-
-name=input("enter your name-" )
-branch=input("enter your branch-")
-gender=input("enter your gender-")
-college=input("enter the name of your college-")
-age=int(input("enter your age-"))
-print("name:",name,"branch:",branch,"gender:",gender,"college:",college,"age:",age)
